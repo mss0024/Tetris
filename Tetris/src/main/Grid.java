8dd66a0d2a7fd2959fbb4b6eac95ab9e6ae363a3
meeksjt <tyler.meeks060@gmail.com>
2016-04-02 14:38:18 -0500

    Trying to fix shtuff

--- Tetris/src/main/Grid.java
+++ Tetris/src/main/Grid.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -74,19 +73,3 @@ private static void clearRow(int rowNum, ArrayList<ArrayList<Tetrimino>> grid){
 }
 
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package main;
-
-/**
- *
- * @author Max
- */
-public class Grid {
-    
-}
->>>>>>> origin/master
