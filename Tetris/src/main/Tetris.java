c3f3a6cabe33de2d6069870c7b9db393d7750290
Mitch <Mitch@Mitch-PC>
2016-04-13 22:25:51 -0500

    Movement now works as well as rotation. Can now redraw stuff. Bounds check works except for rotation.

--- Tetris/src/main/Tetris.java
+++ Tetris/src/main/Tetris.java
@@ -17,25 +17,30 @@ import java.awt.Image;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
 import java.awt.Point;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.io.File;
 import java.util.Random;
-import javax.imageio.ImageIO;
 
 /**
  *
  * @author Mitch
  */
 public class Tetris extends Container{
-    
-    boolean updatePaint = true;
+    //image for the background
     Image backGroundImage;
+    //random seed for generating blocks
     Random rnd = new Random();
+    //the spring layout
     SpringLayout layout = new SpringLayout();
+    //the current block initialized to a random block
     Block current = new Block(rnd.nextInt(7), this, layout);
-    //Block next = new Block(rnd.nextInt(7), this, layout);
+    //the next block initialized to a random block
+    Block next = new Block(rnd.nextInt(7), this, layout);
     //tetriminos 25x25
+    /**
+     * Java paint method that is called at some point after object initialized.
+     * @param g Graphics object that is being painted on
+     */
     @Override
     public void paint(Graphics g) {
         //g.drawImage(backGroundImage, 0, 0, null);
@@ -46,6 +51,7 @@ public class Tetris extends Container{
         //draw the inner rectangels
         g.fillRect(100,100,250,500);
         g.fillRect(372,100,105,105);
+        //paint all of the buttons and labels ect.
         this.paintComponents(g);
         
     }
@@ -61,18 +67,24 @@ public class Tetris extends Container{
             System.out.println("Fail");
         }
         */
+        //get the current keyboard manager.
         KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+        //add our dispacher to the manager
         manager.addKeyEventDispatcher(new MyDispatcher());
-        this.setPreferredSize(new Dimension(700-in.left-in.right,700-in.top-in.bottom));
-        this.setLayout(layout);
         
+        //set the size of our window
+        this.setPreferredSize(new Dimension(700-in.left-in.right,700-in.top-in.bottom));
+        //set the layout to the spring layout
+        this.setLayout(layout);
+        //make or components
         JButton quitGame = new JButton("Exit");
         JLabel score = new JLabel("Score: 0");
         JLabel linesCleared = new JLabel("Lines Cleared: 0");
-                
+        //add them to the pane
         this.add(quitGame);
         this.add(linesCleared);
         this.add(score);
+        //put constraints on our components
         layout.putConstraint(SpringLayout.NORTH, score, 600, SpringLayout.NORTH, this);
         layout.putConstraint(SpringLayout.WEST, score, 370, SpringLayout.WEST, this);
         
@@ -81,9 +93,23 @@ public class Tetris extends Container{
                         
         layout.putConstraint(SpringLayout.NORTH, quitGame, 5, SpringLayout.NORTH, this);
         layout.putConstraint(SpringLayout.EAST, quitGame, -5, SpringLayout.EAST, this);
-        
-        //next.draw(next.getFulcrum(), new Point(13,2));
+        //draw the tetris pecies
+        next.draw(next.getFulcrum(), new Point(13,2));
         current.draw(current.getFulcrum(), new Point(5,0));
+        
+        //add action listener to the quit button(currently does something dumb)
+        quitGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent ae) {
+                layout.removeLayoutComponent(quitGame);
+                Tetris.this.remove(quitGame);
+                Tetris.this.add(quitGame);
+                layout.putConstraint(SpringLayout.NORTH, quitGame, 5, SpringLayout.NORTH, Tetris.this);
+                layout.putConstraint(SpringLayout.WEST, quitGame, 5, SpringLayout.WEST, Tetris.this);
+                revalidate();
+                repaint();
+            }
+        });
         
         this.setVisible(true);
         
@@ -97,35 +123,53 @@ public class Tetris extends Container{
     }
     
     
-    
+    //does something to get all keyboard input idk what a keyeventdispacter is
     class MyDispatcher implements KeyEventDispatcher{
-       @Override
+        /**
+         * Gets keyboard input and acts off of it.
+         * @param e The key that was pressed
+         * @return 
+         */
+        @Override
         public boolean dispatchKeyEvent(KeyEvent e) {
+            //when a key is pressed down
             if (e.getID() == KeyEvent.KEY_PRESSED) {
             } 
+            //when a key is released
             else if (e.getID() == KeyEvent.KEY_RELEASED) {
-                System.out.print(e.getKeyChar());
-                System.out.println(" ," + e.getKeyCode());
                 if(e.getKeyCode() == KeyEvent.VK_LEFT){//left
-                    System.out.println("Left");
+                    //move the piece to the left
                     current.movePiece(current.getFulcrum(),0);
+                    //redraw the piece
                     current.draw(current.getFulcrum(), current.getPosition());
-                    Tetris.this.repaint();
+                    //redraw everything
+                    revalidate();
+                    repaint();
                 }
                 if(e.getKeyCode() == KeyEvent.VK_RIGHT){//right
-                    current.movePiece(current.getFulcrum(),1);
+                    current.movePiece(current.getFulcrum(), 1);
                     current.draw(current.getFulcrum(), current.getPosition());
+                    revalidate();
+                    repaint();
                     
                 }
                 if(e.getKeyCode() == KeyEvent.VK_UP){//up
-                    
+                    current.rotate(current.getFulcrum());
+                    current.draw(current.getFulcrum(), current.getPosition());
+                    revalidate();
+                    repaint();
                 }
                 if(e.getKeyCode() == KeyEvent.VK_DOWN){//down
-                    
+                    current.movePiece(current.getFulcrum(), 2);
+                    current.draw(current.getFulcrum(), current.getPosition());
+                    revalidate();
+                    repaint();
                 }
-            } 
+            }
+            //when a key is pressed and released.  Only of character input for some ungodly reason
             else if (e.getID() == KeyEvent.KEY_TYPED) {
             }
+            //???
             return false;
         }  
    } 
